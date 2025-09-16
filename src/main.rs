a0b2f8ec8b965c280a33b1a8d59b93316c02418b
amir <amirf3894@outlook.com>
2025-09-16 12:28:46 -0700

    it worked !

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use duplicate_remover::fc_generator::hasher::generate_cache;
+use duplicate_remover::fc_generator::models::SearchOptions;
+
 fn main() {
-    println!("Hello, world!");
+    let search_options = SearchOptions::new();
+    generate_cache(search_options).unwrap();
 }
