54e8e61059d46551727b0374356b2712d759afae
amir <amirf3894@outlook.com>
2025-09-18 10:03:25 -0700

    remove by hash initialized

--- src/fc_processor/operations.rs
+++ src/fc_processor/operations.rs
@@ -1,31 +1,67 @@
 use colored::{Color::BrightBlack, Colorize};
-use rusqlite::{Connection, Result};
-use std::path::PathBuf;
-pub fn show_duplicates(path: &str) -> Result<()> {
-    let connection = Connection::open(path)?;
+use rusqlite::{Connection, Result, params};
+pub fn show_duplicates(db_path: &str) -> Result<()> {
+    let connection = Connection::open(db_path)?;
     let mut statement = connection.prepare(
-        "SELECT hash, GROUP_CONCAT(path, '|')
+        "SELECT hash,
+            GROUP_CONCAT(path, '|'),
+            GROUP_CONCAT(name, '|')
          FROM files
          GROUP BY hash
          HAVING COUNT(*) > 1",
     )?;
-    for row in statement.query_map([], |r| Ok((r.get::<_, String>(0)?, r.get::<_, String>(1)?)))? {
+    for row in statement.query_map([], |r| {
+        Ok((
+            r.get::<_, String>(0)?,
+            r.get::<_, String>(1)?,
+            r.get::<_, String>(2)?,
+        ))
+    })? {
         let mut line_counter = 0u16;
 
-        let (hash, paths) = row?;
+        let (hash, paths, names) = row?;
         println!("Hash: {hash}");
         println!("│");
+        let mut name = names.split('|');
         paths.split('|').for_each(|path| {
             line_counter += 1;
-            println!(
-                "├┬─ Name: {}",
-                PathBuf::from(path).file_name().unwrap().to_str().unwrap(),
-            );
+            println!("├┬─ Name: {}", name.next().unwrap(),);
             println!("│└─ Path: {}", path.color(BrightBlack));
             println!("│");
         });
         println!("Repetitions: {line_counter}\n\n");
     }
+    Ok(())
+}
+pub fn remove_by_hash(db_path: &str, hash: &str) -> Result<()> {
+    let connection = Connection::open(db_path)?;
+    let mut statement = connection.prepare(
+        "SELECT path, name
+     FROM files
+     WHERE hash = ?1
+       AND hash IN (
+           SELECT hash
+           FROM files
+           GROUP BY hash
+           HAVING COUNT(*) > 1
+       )",
+    )?;
+    let rows = statement.query_map(params![hash], |row| {
+        Ok((row.get::<_, String>(0)?, row.get::<_, String>(1)?))
+    })?;
+
+    let mut n = 0u16;
+    for r in rows {
+        n += 1;
+        let (path, name) = r?;
+        println!("{n} ─┬─ {name}");
+        println!("   └─ {}\n", path.color(BrightBlack));
+    }
+    if n < 2 {
+        println!("No duplicate files were found with the given hash!");
+        return Ok(());
+    }
+
     Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use duplicate_remover::{
     fc_generator::{self, models::SearchOptions},
-    fc_processor::operations::show_duplicates,
+    fc_processor::operations::{remove_by_hash, show_duplicates},
 };
 use rusqlite::Result;
 
 fn main() -> Result<()> {
     // fc_generator::hasher::generate_cache(SearchOptions::new());
     // Ok(())
-    show_duplicates("files.db").unwrap();
-    Ok(())
+
+    // show_duplicates("files.db").unwrap();
+    // Ok(())
+
+    remove_by_hash(
+        "files.db",
+        "dc148a4ee880ff0beadb6c814b201b0590239d029a8c50eb4b0a7ba0a5983d19",
+    )
 }
